2ffbee1819b3675c98b094a1bdc085117d292604
kaires467 <kaires467@boyslatin.net>
2026-09-03 14:23:40 +0000

    first commit

---
+++ variableActivity.py
@@ -0,0 +1,27 @@
+#VARIABLES-a container that store data. i can
+# name your container almost anything you want so long
+ # as its descriptive.
+
+ name= "Khalil Aires"
+ yearofexp= 0
+ graduate= True
+
+ # when we want to create a variable we make up a word
+ # use the equal sign and then assing it a data type 
+ # whenever the computer sees our variaible word it will
+ # always use ther data inside
+
+# Varible naming rules
+ # 1. Varible must start with a letter and cannot start with a number 
+ # 2. A varible can be a mixed of letters and numbers But not symbols 
+ # 3. A Varible cannot be a python keyword
+
+storeapplecount=50
+mathstudentclass=30
+addedtravalinsurancetrip=100
+goldmembershipgym= 35
+membershipsilvergym=30
+pricedrinkrestuarant= 3
+shortsstacker= 1
+mialen= 2
+stackshot= 100
